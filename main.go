bcfe6826f5a273c05319316211835038a546b2f5
Andreas <Andreas.y45z@gmail.com>
2023-03-07 16:00:23 +0700

    Add middleware CORS

--- main.go
+++ main.go
@@ -16,6 +16,7 @@ func main() {
 
 	e := echo.New()
 	e.Pre(middleware.RemoveTrailingSlash())
+	e.Use(middleware.CORS())
 	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
 		Format: `[${time_rfc3339}] ${status} ${method} ${host}${path} ${latency_human}` + "\n",
 	}))

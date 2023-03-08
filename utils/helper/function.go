62a9b889f387be50bbc02101b04784f4a75afc72
Andreas <Andreas.y45z@gmail.com>
2023-03-09 05:59:48 +0700

    Re-push file

--- utils/helper/function.go
+++ utils/helper/function.go
@@ -8,6 +8,30 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
+type Helpers_ interface {
+	LimitOffsetConvert(page, limit int) (int, int)
+	ExtractIDParam(c echo.Context) (int, error)
+	ExtractPageLimit(c echo.Context) (page int, limit int, err error)
+}
+
+type Helper struct{}
+
+func (h *Helper) LimitOffsetConvert(page, limit int) (int, int) {
+	return LimitOffsetConvert(page, limit)
+}
+
+func (h *Helper) ExtractIDParam(e echo.Context) (int, error) {
+	return ExtractIDParam(e)
+}
+
+func (h *Helper) ExtractPageLimit(e echo.Context) (page int, limit int, err error) {
+	return ExtractPageLimit(e)
+}
+
+func NewHelper() Helpers_ {
+	return &Helper{}
+}
+
 func LimitOffsetConvert(page, limit int) (int, int) {
 	offset := -1
 	if limit > 0 {

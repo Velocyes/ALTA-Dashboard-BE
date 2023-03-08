fcf08776851ff04968b270b2d32d564d66093e66
Andreas <Andreas.y45z@gmail.com>
2023-03-09 05:57:23 +0700

    Code refactoring for features and users

---
+++ utils/helper/bad_response.go
@@ -0,0 +1,27 @@
+package helper
+
+import (
+	"alta-dashboard-be/utils/consts"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"gorm.io/gorm"
+)
+
+func ValidateUserFailedResponse (c echo.Context,  err error) (codeStatus int, failedMessage string) {
+	if err.Error() == consts.DATABASE_InvaildQueryParameter {
+		return http.StatusBadRequest, consts.DATABASE_InvaildQueryParameter
+	} else if err.Error() == consts.VALIDATION_InvalidInput {
+		return http.StatusBadRequest, consts.VALIDATION_InvalidInput
+	} else if err.Error() == consts.SERVER_ForbiddenRequest {
+		return http.StatusBadRequest, consts.SERVER_ForbiddenRequest
+	} else if err.Error() == consts.USER_EmptyCredentialError {
+		return http.StatusBadRequest, consts.USER_EmptyCredentialError
+	}  else if err.Error() == gorm.ErrRecordNotFound.Error() {
+		return http.StatusNotFound, gorm.ErrRecordNotFound.Error()
+	} else if err.Error() == consts.USER_EmailAlreadyUsed {
+		return http.StatusBadRequest, consts.USER_EmailAlreadyUsed
+	}
+	return http.StatusInternalServerError, err.Error()
+}
+

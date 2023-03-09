ec00e660a5f99fc68c91987be786291837ea49fd
Andreas <Andreas.y45z@gmail.com>
2023-03-09 12:11:48 +0700

    Fix type enum for mentees and logs

--- features/logs/models/log.go
+++ features/logs/models/log.go
@@ -7,7 +7,7 @@ import (
 type Log struct {
 	gorm.Model
 	Title    string `gorm:"not null;type:varchar(50)"`
-	Status   string `gorm:"type:enum('Interview', 'Join Class', 'Continue Unit 2', 'Continue Unit 3', 'Eliminated', 'Join Class', 'Placement', Repeat Unit 1, Repeat Unit 2, Repeat Unit 3);default:'None';not null"`
+	Status   string `gorm:"type:enum('Interview', 'Join Class', 'Continue Unit 2', 'Continue Unit 3', 'Eliminated', 'Placement', 'Repeat Unit 1', 'Repeat Unit 2', 'Repeat Unit 3');default:'None';not null"`
 	Feedback string `gorm:"not null;type:text"`
 	UserID   uint   `gorm:"not null;foreignKey"`
 	MenteeID uint   `gorm:"not null;foreignKey"`
